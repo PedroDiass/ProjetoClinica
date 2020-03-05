fef62007f626eb2e827ae899458ace7084d5856e
Pedro Dias Rosendo <pedro.dias@souunit.com.br>
2020-03-05 20:40:58 -0300

    Create Atendimento.dart

---
+++ flutter_app/lib/com/pepos/model/Atendimento.dart
@@ -0,0 +1,12 @@
+
+class Atendimento {
+  int _id;
+  DateTime _dataInicio;
+  DateTime _dataFim;
+  boolean _atendePlano;
+  boolean _atendeDia;
+  
+  Medico _medico;
+  DiaSemana _diaSemana;
+  Consulta _consulta;
+}
